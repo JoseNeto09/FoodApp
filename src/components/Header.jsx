38596246ad4fc118cba5e317a5c566c282871609
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-09-23 12:46:22 -0300

    Navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,30 @@
+import foody from "../assets/images/foody.png";
+import cartIcon from "../assets/icons/cart.svg";
+import { Link } from "react-router-dom";
+
+
+
 export const Header = () =>{
-    return(
-        <nav>
-            <div id="header" className="bg-black text-white">
-             <div className="w-full container mx-auto flex flex-wrap items-center justify-content-between mt-0 py-2">
-                   <div className="logo-wrapper pl-4 flex items-center">
-                    <img src="" alt="logo" />
+    return (
+        <nav id="header" className="bg-black text-white">
+            <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+                <div className="logo-wrapper pl-4 flex items-center">
+                    <Link to="/" className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
+                        <img src={foody} alt="logo" className="w-40 h-40 object-cover"/>
+                    </Link>
                 </div>
-                <div className="nav-menu-wrapper flex items-center justify-between space-x-10"></div>
-                    <div>Home</div>
-                    <div>About</div>
+                <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
+                    <Link to="/" className="text-xl">Home</Link>
+                    <Link to="#about" className="text-xl">About</Link>
+                </div>
+                <div className="flex items-center justify-center space-x-4">
+                    <Link to="/cart" className="mr-4 relative">
+                        <img src={cartIcon} alt="cart"/>
+                    </Link>
+                    <Link to="/login">Log in</Link>
+                    <Link to="/register">Sig up</Link>
+                </div>
             </div>
-            <div className="flex items-center justify-center space-x-4">
-                <div>cart</div>
-                <div>login</div>
-                <div>Sign Up</div>
-            </div>
-            </div>
-        </nav>  
+        </nav>
     )
 }
